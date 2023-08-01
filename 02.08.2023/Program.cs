4a8ae38cfa164ffdc9c1a9675b1addd1af5307f2
Samirkhgcode <138569634+Samirkhgcode@users.noreply.github.com>
2023-08-02 02:44:55 +0400

    added folder

---
+++ 02.08.2023/Program.cs
@@ -0,0 +1,231 @@
+﻿
+
+
+
+using System.Collections;
+
+//ArrayList arrayList = new();
+
+//arrayList.Add(5);
+//arrayList.Add("Salam");
+
+//foreach (var item in arrayList)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+
+//Hashtable hashtable = new Hashtable();
+
+//hashtable.Add(1, "Samir");
+//hashtable.Add(2, "Fidan");
+//hashtable.Add(3, "Kubra");
+
+//foreach (DictionaryEntry item in hashtable)
+//{
+//    Console.WriteLine(item.Value);
+//}
+
+//SortedList sortedList = new SortedList();
+
+//sortedList.Add(5, "salam5");
+//sortedList.Add(3, "salam3");
+//sortedList.Add(1, "salam1");
+
+//foreach(DictionaryEntry item in sortedList)
+//{
+//    Console.WriteLine(item.Key);
+//}
+
+
+
+
+
+//sortedList.Add("Az", "salam5");
+//sortedList.Add("Ca", "salam3");
+//sortedList.Add("Ba", "salam1");
+
+//foreach (DictionaryEntry item in sortedList)
+//{
+//    Console.WriteLine(item.Key);
+//}
+
+
+
+
+
+//SortedList<int, string> genericSortList = new SortedList<int, string>();
+
+//genericSortList.Add(1, "sas");
+//genericSortList.Add(2, "dsffddf");
+
+//foreach (KeyValuePair<int, string> item in genericSortList)
+//{
+//    Console.WriteLine(item.Value);
+//}
+
+
+//Dictionary<int, int> dictonary = new Dictionary<int, int>();
+
+//dictonary.Add(1, 2);
+//dictonary.Add(2, 5);
+ 
+//foreach (KeyValuePair<int,int> item in dictonary)
+//{
+//    Console.WriteLine(item.Value);
+
+//}
+
+
+//Dictionary<string,string> users = new Dictionary<string,string>();
+
+//users.Add("admin", "Cahangir");
+//users.Add("superAdmin", "Seref");
+//users.Add("member", "Pervin");
+
+//foreach (KeyValuePair<string,string> item in users)
+//{
+//    if (item.Key == "admin")
+//    {
+//        Console.WriteLine(item.Value);
+//    }
+
+//}
+
+
+//Stack<int> stack = new Stack<int>();
+
+//stack.Push(1);
+//stack.Push(10);
+//stack.Push(4);
+
+//foreach (var item in stack)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+//Queue<int> queue = new Queue<int>();
+
+//queue.Enqueue(1);
+//queue.Enqueue(10);
+//queue.Enqueue(4);
+
+//foreach (var item in queue)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+
+
+List<int> nums = new List<int>();
+
+nums.Add(1);
+nums.Add(2);
+nums.Add(3);
+nums.Add(6);
+
+
+//List<int> nums2 = new List<int>();
+
+//nums.Add(1);
+//nums.Add(2);
+//nums.Add(4);
+//nums.Add(88);
+
+
+//nums.AddRange(nums2);
+
+//foreach ( var item in nums )
+//{
+//    Console.WriteLine(item);
+//}
+
+
+//foreach (var item in nums)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+//int[] arr = { 1, 2, 3, 4, };
+
+//var data = arr.ToList();
+
+
+
+//var result = nums.ToArray ();
+
+
+//Console.WriteLine(nums.Count);
+
+//for (int i = 0; i < nums.Count; i++)
+//{
+//    Console.WriteLine(nums[i]);
+//}
+
+//nums.Remove(6);
+
+
+//var result = nums.FindAll(m => m == 5);
+
+//foreach (var item in result)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+//var result = nums.FirstOrDefault(m=>m==5);
+
+//Console.WriteLine(result);
+
+
+
+//var result = nums.Exists(x => x == 1);
+
+//Console.WriteLine(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
